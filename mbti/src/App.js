efe6c345cb49c3aef486b86c33784c273d58fad2
JunWoo Lee <junwoo1357@gmail.com>
2024-10-14 23:21:21 +0900

    counter 연습

--- mbti/src/App.js
+++ mbti/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Counter from "./pages/Counter";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -7,11 +8,13 @@ function App() {
     <div className="App">
       <nav>
         <Link to="/">Home</Link> ||
-        <Link to="/about">About</Link>
+        <Link to="/about">About</Link>||
+        <Link to="/counter">Counter</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/counter" element={<Counter />}></Route>
       </Routes>
     </div>
   );
